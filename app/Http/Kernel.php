5dc1d2ca9e2fc60ff44d202c60a3fc13e9f23134
OmarNasab <77580153+OmarNasab@users.noreply.github.com>
2024-08-13 00:07:06 +0300

    fix a bug in the check role middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'CheckAdminRole' => \App\Http\Middleware\CheckGuideRole::class,
+        'CheckAdminRole' => \App\Http\Middleware\CheckAdminRole::class,
         'CheckGuideRole' => \App\Http\Middleware\CheckGuideRole::class,
     ];
 }
